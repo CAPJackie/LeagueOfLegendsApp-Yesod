5f99f2d638a09f2e515c82c654459c3dc07fb7ff
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2019-03-19 16:02:10 -0500

    data.sql file created to insert new data on created tables

---
+++ data.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.video(
+	id, champion, name, url)
+	VALUES (1, 1, '5 things every pro Aatrox should know', 'https://www.youtube.com/watch?v=01RcyrBqj2w'),
+			(2, 2, 'The ONLY Akali guide youll ever need', 'https://www.youtube.com/watch?v=JBBNPSaBjwM');
+
+INSERT INTO public.champion(
+	id, id_name)
+	VALUES (1, 'Aatrox'), (2, 'Akali');
