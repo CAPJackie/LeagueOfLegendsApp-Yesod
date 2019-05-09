ba8b64c7a13075cf95d4199e73bfaa0e967574bd
Diego Borrero <die.9511@gmail.com>
2019-05-09 04:44:38 -0500

    Profile try

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -7,9 +7,16 @@ module Handler.Profile where
 
 import Import
 
+import Database.Persist.Sql (rawSql, unSqlBackendKey)
+
 getProfileR :: Handler Html
 getProfileR = do
     (_, user) <- requireAuthPair
+    profileInfo <- getUserProfile $ userIdent user
     defaultLayout $ do
         setTitle . toHtml $ userIdent user <> "'s User page"
         $(widgetFile "profile")
+
+getUserProfile :: Text -> Handler [Entity Profile]
+getUserProfile t = runDB $ rawSql s [toPersistValue t]
+    where s = "SELECT ?? FROM profile WHERE profile.user = ?"
